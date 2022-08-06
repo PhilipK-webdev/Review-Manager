35d1ee2e79a2b053236e726896f29d1136853ed4
PhilipK-webdev <philipk179@gmail.com>
2022-08-06 11:17:03 +0300

    add:start

---
+++ server/server.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const mongoose = require("mongoose");
+const PORT = process.env.PORT || 5000;
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost:27017/TeraGen";
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("you are connected to mongodb"))
+  .catch((err) => console.log(err));
+
+// const apiRoutes = require("./routes/api-routes");
+// app.use(apiRoutes);
+
+app.get("/user", async (req, res) => {
+  const response = await mongoose.connection.db
+    .collection("TeraGen")
+    .find({})
+    .toArray();
+  res.json(response).status(200);
+});
+
+// const response = await mongoose.connection.db
+// .collection("Offices")
+// .aggregate([
+//   // { $unwind: "$offices" },
+//   { $match: { officeNumber: `${req.params.id}` } },
+//   // { $replaceRoot: { "newRoot": "$offices" } }
+// ])
+// .toArray();
+
+/** 
+ * await mongoose.connection.db
+                .collection("Offices")
+                .findOneAndUpdate(
+                  { officeNumber: item.officeNumber },
+                  { $set: { "seats.$[element].employee": obj } },
+                  {
+                    multi: true,
+                    arrayFilters: [{ "element.type": "travelers" }],
+                  },
+                  { w: 0 },
+                  (err, result) => {
+                    err
+                      ? console.log("error")
+                      : console.log("Employees update Successfully");
+                  }
+                );
+ * 
+*/
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`listening at http://localhost:${PORT}`);
+});
